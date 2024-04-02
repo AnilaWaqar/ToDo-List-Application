41b1d5a1a4f75b941f0a28277211465dfa6c9a29
AnilaWaqar <160339061+AnilaWaqar@users.noreply.github.com>
2024-04-01 21:07:06 -0700

    ToDo List Application

---
+++ app.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos = [];
+let condition = true;
+console.log(chalk.magenta.bold("\n \t Wellcome To Anila Waqar - ToDo List Application\n"));
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.red("What you want to add in your Todos?"),
+        },
+    ]);
+    todos.push(addTask.task);
+    console.log(chalk.green(`${addTask.task} Task added in todos List Successfully`));
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "AddMore",
+            type: "confirm",
+            message: chalk.blue("Do you want to addMore in your Todos?"),
+            default: "False"
+        },
+    ]);
+    condition = addMoreTask.AddMore;
+}
+console.log(chalk.yellow("Your Updated Todos List:"), todos);
